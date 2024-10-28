9a7227a6c2d27882a9be7feb03654babe8acd4da
xili22 <xiaozhou.li@oulu.fi>
2024-10-28 17:16:05 +0200

    Added 2 tests

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -7,5 +7,24 @@ from frame import Frame
 
 class TestBowlingGame(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_game_created(self):
+        f = Frame(1, 5)
+        game = BowlingGame()
+        game.add_frame(f)
+        self.assertEqual(f, game.get_frame_at(0))
+
+    def test_game_created_10_frames(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        f = Frame(2,6)
+        game.add_frame(f)
+        # [1, 5] [3, 6] [7, 2] [3, 6] [4, 4] [5, 3] [3, 3] [4, 5] [8, 1] [2, 6]
+        self.assertEqual(f, game.get_frame_at(9))
